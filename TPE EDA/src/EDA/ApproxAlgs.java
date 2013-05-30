37b8d94d22772d2d560116bfc4c0d65494b71290
Jorge <Jorge@Jorge-PC>
2013-05-30 20:46:17 -0300

    Supuestamente encuentra un camino con el criterio, pero tengo miedo de probarlo. Falta la otra parte (la más dificil parece)

--- TPE EDA/src/EDA/ApproxAlgs.java
+++ TPE EDA/src/EDA/ApproxAlgs.java
@@ -9,40 +9,48 @@ import java.util.Queue;
 public class ApproxAlgs {
 
 	public static void approxAlg1(HyperGraph graph, Node top, Node root, int time) {
-		List<HyperArc> minpath = new ArrayList<HyperArc>();		
+		List<HyperArc> minpath = new ArrayList<HyperArc>();
+		Queue<HyperArc> auxarcs = new LinkedList<HyperArc>();
 		HyperArc arc;
+		int pathweight = 0;
 		Iterator<Node> nodesit;
 		Node node = root; 
-		Iterator<HyperArc> pathit;
 
 
-		minArc(node, minpath);
+		arc = minArc(node, minpath);
 		minpath.add(arc);
+		auxarcs.add(arc);
 		
-			while(auxarcs.isEmpty()){ 
+			while(!auxarcs.isEmpty()){ 
 				arc = auxarcs.remove();
 				nodesit =  arc.tails.iterator();
 				while(nodesit.hasNext()){
 					node = nodesit.next();
-					minArc(node, minpath);
+					HyperArc nodeminarc = minArc(node,minpath);
+					if(!nodeminarc.isMarked()){
+						auxarcs.add(nodeminarc);
+						minpath.add(nodeminarc);
+						nodeminarc.mark();
+						pathweight += nodeminarc.weight;
+					}
 				}
 			}
 	}
 
-	private static boolean minArc(Node node, List<HyperArc> minpath){
+	private static HyperArc minArc(Node node, List<HyperArc> minpath){
 		Iterator<HyperArc> arcsIt = node.tails.iterator();
 		Boolean ready = false;
 		HyperArc arc = new HyperArc(0, null);;
 		HyperArc minarc = new HyperArc(0, null);
-		if(!arcsIt.hasNext())
-			return false;
+		//if(!arcsIt.hasNext()) ESTO NO LO TIENE QUE HACER NUNCA PORQUE SI NO EL NODO SERÍA RAIZ
+//			return false;
 
 		while(arcsIt.hasNext() && !ready){//ESTO ENCUENTRA EL MÍNIMO ARCO  O UNO QUE YA ESTÉ MARCADO ARRIBA DE UN NODO
 			arc = arcsIt.next();
 			if(arc.isMarked()){
 				//if(arc != minarc) NO SE SI HACE FALTA ESTO, MEPA QUE NO
 				minarc.unmark();
-				ready = true;
+				return arc;
 			}
 			else{
 				if(minarc == null)
@@ -53,11 +61,9 @@ public class ApproxAlgs {
 					minarc = arc;							
 				}
 			}
-			arc = arcsIt.next();
 		}
-
-		minpath.add(arc);
-		return true;
+		minarc.unmark();
+		return minarc;
 	}
 
 }
